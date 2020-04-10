5a0d933cc3adcf874ae01aab58e51b04db67bc84
thom9346 <holmegaard1996@gmail.com>
2020-04-10 10:18:10 +0200

    Fixed image upload. It now uploads to Assets/Images. It is able to server pictures from there.

--- KennelIndexer.API/Helpers/Uploader.cs
+++ KennelIndexer.API/Helpers/Uploader.cs
@@ -6,36 +6,39 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
 namespace KennelIndexer.API.Helpers
 {
-    public class Uploader
+public class Uploader
+{
+    //needs to be async / await or the picture wont upload completely
+    public async Task<string> UploadPictures(List<IFormFile> files)
     {
-        //needs to be async / await or the picture wont upload completely
-        public async Task<string> UploadPictures(List<IFormFile> files)
+        long size = files.Sum(f => f.Length);
+
+        //var folderName = Path.Combine("Resources", "Images");
+        //var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+        //path on laptop....
+        var pathToSave = "C://Users//THolm//source//repos//KennelIndexFRONTEND//KennelIndexer//kennelApp//src//assets//images//";
+        var fileExtension = Path.GetExtension(files.FirstOrDefault().FileName);
+        var fileName = Guid.NewGuid() + fileExtension;
+        var fullPath = Path.Combine(pathToSave, fileName);
+
+        foreach (var formFile in files)
         {
-            long size = files.Sum(f => f.Length);
-
-            var folderName = Path.Combine("Resources", "Images");
-            //var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-
-            var pathToSave = "C://Users//Thomas//source//repos//KennelIndexerFrontendV2//KennelIndexer//kennelApp//src//assets//images//";
-            var fileExtension = Path.GetExtension(files.FirstOrDefault().FileName);
-            var fileName = Guid.NewGuid() + fileExtension;
-            var fullPath = Path.Combine(pathToSave, fileName);
-
-            foreach (var formFile in files)
+            if (formFile.Length > 0)
             {
-                if (formFile.Length > 0)
+                using (var stream = System.IO.File.Create(fullPath))
                 {
-
-                    using (var stream = System.IO.File.Create(fullPath))
-                    {
-                         await formFile.CopyToAsync(stream);
-                    }
+                     await formFile.CopyToAsync(stream);
                 }
             }
-
-            return fullPath;
         }
+       return fileName;
+     }
     }
+
 }
