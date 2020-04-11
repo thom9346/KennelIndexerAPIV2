c4215d8b45ba1320592aa58eb0591c01caad8187
thom9346 <holmegaard1996@gmail.com>
2020-04-11 12:04:32 +0200

    Uploading pictures now work as intended. The files are served with the startup.cs class, so I can access them in the client-side-application.

--- KennelIndexer.API/Helpers/Uploader.cs
+++ KennelIndexer.API/Helpers/Uploader.cs
@@ -14,31 +14,57 @@ namespace KennelIndexer.API.Helpers
 {
 public class Uploader
 {
-    //needs to be async / await or the picture wont upload completely
-    public async Task<string> UploadPictures(List<IFormFile> files)
+        //needs to be async / await or the picture wont upload completely
+public async Task<string> UploadPictures(List<IFormFile> files)
+{
+    long size = files.Sum(f => f.Length);
+
+    var folderName = Path.Combine("Resources", "Images");
+    var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+    var fileExtension = Path.GetExtension(files.FirstOrDefault().FileName);
+    var fileName = Guid.NewGuid() + fileExtension;
+    var fullPath = Path.Combine(folderName, fileName);
+
+    foreach (var formFile in files)
     {
-        long size = files.Sum(f => f.Length);
-
-        //var folderName = Path.Combine("Resources", "Images");
-        //var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-        //path on laptop....
-        var pathToSave = "C://Users//THolm//source//repos//KennelIndexFRONTEND//KennelIndexer//kennelApp//src//assets//images//";
-        var fileExtension = Path.GetExtension(files.FirstOrDefault().FileName);
-        var fileName = Guid.NewGuid() + fileExtension;
-        var fullPath = Path.Combine(pathToSave, fileName);
-
-        foreach (var formFile in files)
+        if (formFile.Length > 0)
         {
-            if (formFile.Length > 0)
+
+            using (var stream = System.IO.File.Create(fullPath))
             {
-                using (var stream = System.IO.File.Create(fullPath))
-                {
-                     await formFile.CopyToAsync(stream);
-                }
+                await formFile.CopyToAsync(stream);
             }
         }
-       return fileName;
-     }
     }
+
+    return fullPath;
+    }
+}
+    //public async Task<string> UploadPictures(List<IFormFile> files)
+    //{
+    //    long size = files.Sum(f => f.Length);
+
+    //    //var folderName = Path.Combine("Resources", "Images");
+    //    //var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+    //    //path on laptop....
+    //    var pathToSave = "C://Users//THolm//source//repos//KennelIndexFRONTEND//KennelIndexer//kennelApp//src//assets//images//";
+    //    var fileExtension = Path.GetExtension(files.FirstOrDefault().FileName);
+    //    var fileName = Guid.NewGuid() + fileExtension;
+    //    var fullPath = Path.Combine(pathToSave, fileName);
+
+    //    foreach (var formFile in files)
+    //    {
+    //        if (formFile.Length > 0)
+    //        {
+    //            using (var stream = System.IO.File.Create(fullPath))
+    //            {
+    //                 await formFile.CopyToAsync(stream);
+    //            }
+    //        }
+    //    }
+    //   return fileName;
+    // }
+    //}
 
 }
--- KennelIndexer.API/Startup.cs
+++ KennelIndexer.API/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -7,11 +8,13 @@ using KennelIndexer.API.DbContexts;
 using KennelIndexer.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -57,6 +60,12 @@ namespace KennelIndexer.API
                 builder.WithOrigins("http://localhost:4200")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
+            });
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Resources")),
+                RequestPath = new PathString("/Resources")
             });
             //app.UseHttpsRedirection();
 
